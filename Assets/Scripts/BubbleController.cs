ff4f1311517dec24c5230eead29fed33e3413854
vororo98 <47042311+vororo98@users.noreply.github.com>
2025-01-25 08:07:18 +0100

    Basic controller with camera

--- Assets/Scripts/BubbleController.cs
+++ Assets/Scripts/BubbleController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class BubbleController : MonoBehaviour
@@ -5,6 +6,8 @@ public class BubbleController : MonoBehaviour
 
     Rigidbody rb;
     float speed = 10;
+    float maxSpeed = 100;
+    float fallSpeed = 3;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,15 +16,28 @@ public class BubbleController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetButton("horizontal"))
+        if (Input.GetButton("Horizontal"))
         {
-            rb.AddForce(new Vector3(Input.GetAxis("horizontal") * speed, 0, 0));
+            rb.AddForce(new Vector3(Input.GetAxis("Horizontal") * speed, 0, 0), ForceMode.Acceleration);
         }
-        if (Input.GetButton("vertical"))
+        if (Input.GetButton("Vertical"))
         {
-            rb.AddForce(new Vector3(0, Input.GetAxis("vertical") * speed, 0));
+            rb.AddForce(new Vector3(0, 0, Input.GetAxis("Vertical") * speed), ForceMode.Acceleration);
+        }
+        if (Input.GetButton("Jump"))
+        {
+            rb.AddForce(new Vector3(0, speed, 0), ForceMode.Acceleration);
+        }
+        else
+        {
+            rb.AddForce(new Vector3(0, -fallSpeed, 0), ForceMode.Acceleration);
+        }
+
+        if (rb.linearVelocity.magnitude > maxSpeed)
+        {
+            rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
         }
     }
 }
