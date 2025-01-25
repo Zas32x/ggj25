8ddd4650a9a1ccbbc712d546552e80c7cb324247
vororo98 <47042311+vororo98@users.noreply.github.com>
2025-01-25 13:48:36 +0100

    WIP Furniture movement

--- Assets/Scripts/FurnitureBehaviour.cs
+++ Assets/Scripts/FurnitureBehaviour.cs
@@ -6,7 +6,14 @@ public class FurnitureBehaviour : MonoBehaviour
     [SerializeField] Collider coll;
     GameObject container;
     Rigidbody rb;
-    float floatForce = 50;
+    float floatForce = 45;
+
+    bool underCenter = false;
+    float underCenterDrag = 3;
+    float underCenterAngularDrag = 1;
+    float overCenterDrag = 0;
+    float overCenterAngularDrag = 0.05f;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,9 +26,26 @@ public class FurnitureBehaviour : MonoBehaviour
     {
         if (container != null)
         {
+            //float heightDifference = transform.position.y - container.GetComponent<Collider>().bounds.center.y;
+
+            //if (heightDifference < 0)
+            //{
+            //    rb.AddForceAtPosition(Vector3.up * floatForce * Mathf.Abs(heightDifference), transform.position, ForceMode.Force);
+            //    if (!underCenter)
+            //    {
+            //        underCenter = true;
+            //        SwitchFloatMode(underCenter);
+            //    }
+            //}
+            //else if (underCenter)
+            //{
+            //    underCenter = false;
+            //    SwitchFloatMode(underCenter);
+            //}
+
             Debug.Log("Floating");
             rb.AddForce((container.transform.position - transform.position) * floatForce, ForceMode.Acceleration);
-            rb.AddForce(container.GetComponent<Rigidbody>().linearVelocity, ForceMode.Acceleration);
+            rb.AddForce(container.GetComponent<Rigidbody>().linearVelocity, ForceMode.Force);
         }
     }
 
@@ -32,6 +56,7 @@ public class FurnitureBehaviour : MonoBehaviour
             if (other.bounds.Contains(coll.bounds.max) && other.bounds.Contains(coll.bounds.min))
             {
                 container = other.gameObject;
+                //rb.useGravity = false;
             }
         }
     }
@@ -41,6 +66,21 @@ public class FurnitureBehaviour : MonoBehaviour
         if (other.tag == "Player")
         {
             container = null;
+            //rb.useGravity = true;
+        }
+    }
+
+    private void SwitchFloatMode(bool isUnderCenter)
+    {
+        if (isUnderCenter)
+        {
+            rb.linearDamping = underCenterDrag;
+            rb.angularDamping = underCenterAngularDrag;
+        }
+        else
+        {
+            rb.linearDamping = overCenterDrag;
+            rb.angularDamping = overCenterAngularDrag;
         }
     }
 }
